144cc83a201a3a769bf127170ee4c3662d7f6c53
gaazoo <gaazoo@yandex.ru>
2024-05-13 21:30:02 +0700

    Parse add

--- Nmap_scan.py
+++ Nmap_scan.py
@@ -1,11 +1,20 @@
 import subprocess
-#Функция для сканирования цели.
 def scan(target):
     try:
         result = subprocess.check_output(["nmap", "-sV", target])
+        # Декодирование байтовой строки в UTF-8
+        result = result.decode('utf-8')
         return result
     except subprocess.CalledProcessError as e:
         print("Ошибка при выполнении nmap: ", e)
+# Распарсим результат nmap
+def parse_nmap_result(result):
+    # Бьем результат на строки и обрабатываем каждую
+    for line in result.split('\n'):
+        if "open" in line:
+            # Дополнительная обработка для строк, содержащих 'open'
+            print(line)
 target = input("Введите цель для сканирования: ")
 result = scan(target)
-print(result)
+print(result) # Печать исходного результата
+parse_nmap_result(result) # Печать обработанного результата
